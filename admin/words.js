7eed5efa6c291ba0bc49a710468d4b8913de99e0
Chris <c.meul@gmx.de>
2021-07-04 18:43:39 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'gpsd-client adapter settings': {
+		'en': 'Adapter settings for gpsd-client',
+		'de': 'Adaptereinstellungen für gpsd-client',
+		'ru': 'Настройки адаптера для gpsd-client',
+		'pt': 'Configurações do adaptador para gpsd-client',
+		'nl': 'Adapterinstellingen voor gpsd-client',
+		'fr': "Paramètres d'adaptateur pour gpsd-client",
+		'it': "Impostazioni dell'adattatore per gpsd-client",
+		'es': 'Ajustes del adaptador para gpsd-client',
+		'pl': 'Ustawienia adaptera dla gpsd-client',
+		'zh-cn': 'gpsd-client的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'option1',
+		'ru': 'option1',
+		'pt': 'option1',
+		'nl': 'option1',
+		'fr': 'option1',
+		'it': 'option1',
+		'es': 'option1',
+		'pl': 'option1',
+		'zh-cn': 'option1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'option2',
+		'ru': 'option2',
+		'pt': 'option2',
+		'nl': 'option2',
+		'fr': 'option2',
+		'it': 'option2',
+		'es': 'option2',
+		'pl': 'option2',
+		'zh-cn': 'option2'
+	}
+};
